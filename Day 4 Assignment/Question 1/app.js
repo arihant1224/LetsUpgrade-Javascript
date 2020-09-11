391af84aa72cb6a653bb2d5e9047bef91165285e
arihant1224 <66207795+arihant1224@users.noreply.github.com>
2020-09-11 14:56:33 +0530

    Adding files

---
+++ Day 4 Assignment/Question 1/app.js
@@ -0,0 +1,14 @@
+//Button-1
+document.getElementById("btn-1").addEventListener("click", function(){
+    document.getElementById("images").setAttribute("src", "Images/lambo.jpg");
+});
+
+//Button-2
+document.getElementById("btn-2").addEventListener("click", function(){
+    document.getElementById("images").setAttribute("src", "Images/jaguar.jpg");
+});
+
+//Button-3
+document.getElementById("btn-3").addEventListener("click", function(){
+    document.getElementById("images").setAttribute("src", "Images/audi.jpg");
+});
