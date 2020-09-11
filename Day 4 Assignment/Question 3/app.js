b57bedee3074b6c5d8f3b5ee2274962adf2a969c
arihant1224 <66207795+arihant1224@users.noreply.github.com>
2020-09-11 15:08:53 +0530

    Adding Files

---
+++ Day 4 Assignment/Question 3/app.js
@@ -0,0 +1,22 @@
+var arr = [
+    {
+        name: "Arihant",
+        age: 19,
+        country: "India",
+        hobbies: ["Painting", "Dancing", "Singing"]
+    },
+    {
+        name: "Manvi",
+        age: 18,
+        country: "India",
+        hobbies: ["Painting", "Singing"]
+    },
+    {
+        name: "Bhavya",
+        age: 17,
+        country: "India",
+        hobbies: ["Painting", "Dancing"]
+    }
+];
+
+console.log(arr);
